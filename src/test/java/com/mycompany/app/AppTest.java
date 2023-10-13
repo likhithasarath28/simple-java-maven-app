b2956937fbd4832c96f7327acd2fe3c8aef827e2
likhithasarath28 <147787511+likhithasarath28@users.noreply.github.com>
2023-10-13 13:02:22 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -36,7 +36,7 @@ public class AppTest
         try {
             assertEquals("Hello World! I am likhitha" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World!\"");
+            fail("\"message\" is not \"Hello World! I am likhitha\"");
         }
     }
 
